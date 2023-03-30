5cbe32d7246cc684d808ca48f437311fbc12ef00
Taeheon <hunnykevin91@naver.com>
2023-03-30 09:07:16 +0900

    백준 문제풀이

--- Python/백준_단계별_문제풀이/1차원 배열/공 넣기.py
+++ Python/백준_단계별_문제풀이/1차원 배열/공 넣기.py
@@ -1,3 +1,12 @@
 N , M = map(int, input().split())
+basket = [0] * N
 
-for i in range(M):
+for l in range(M):
+    i, j, k = map(int, input().split())
+    for n in range(i-1, j):
+        basket[n] = k
+
+for i in range(N):
+    print(basket[i], end=" ")
+
+# end=" "는 현재 속한 출력물을 그 다음 출력값과 이어지게 한다
