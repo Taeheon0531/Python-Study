31682ed7f03f1f665b5511d73087ca184f730b11
Taeheon <hunnykevin91@naver.com>
2023-04-14 16:33:13 +0900

    백준 문제풀이

--- Python/백준_단계별_문제풀이/심화1/크로아티아 알파벳.py
+++ Python/백준_단계별_문제풀이/심화1/크로아티아 알파벳.py
@@ -1,2 +1,8 @@
-# no answer
-#still
+# 다시 풀어볼만한 문제
+
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia:
+    word = word.replace(i, '*')
+print(len(word))
